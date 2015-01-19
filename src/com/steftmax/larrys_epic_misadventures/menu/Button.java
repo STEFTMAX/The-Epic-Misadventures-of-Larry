ef380004d6d160d3aea0adb968afa7560e26b471
pieter3457 <pieteratmail@gmail.com>
2015-01-19 22:53:52 +0100

    create basic button class, totally unfinished

---
+++ src/com/steftmax/larrys_epic_misadventures/menu/Button.java
@@ -0,0 +1,13 @@
+package com.steftmax.larrys_epic_misadventures.menu;
+
+import com.steftmax.larrys_epic_misadventures.input.MouseInput;
+
+/**
+ * @author pieter3457
+ *
+ */
+public class Button {
+	public Button(MouseInput mi, Action onPressedAction) {
+		//TODO with events and stuff via mouse input
+	}
+}
