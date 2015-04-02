1e4cdc304f76dd03139aa2a2ed43cd200b7f94d3
pieter3457 <pieteratmail@gmail.com>
2015-04-02 11:24:11 +0200

    Start on spritegroup class

---
+++ src/com/steftmax/temol/graphics/sprite/SpriteGroup.java
@@ -0,0 +1,70 @@
+package com.steftmax.temol.graphics.sprite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.steftmax.temol.graphics.Drawable;
+import com.steftmax.temol.graphics.SpriteBatch;
+import com.steftmax.temol.math.Vector2;
+
+/**
+ * @author pieter3457 Represents a group of Sprites
+ */
+public class SpriteGroup implements Drawable {
+
+	private List<Sprite> sprites = new ArrayList<Sprite>();
+	Vector2 position = new Vector2();
+	float scaleX = 1f, scaleY = 1f;
+	boolean flipX = false, flipY = false;
+	float rotation = 0f;
+	Vector2 origin = new Vector2();
+
+	public SpriteGroup(float x, float y) {
+		this.position = new Vector2(x, y);
+	}
+
+	public SpriteGroup(Vector2 position) {
+		this.position = position;
+	}
+
+	public void setOrigin(float x, float y) {
+		origin.set(x, y);
+	}
+
+	/**
+	 * Adds a sprite to this group. It will be the last to draw.
+	 * @param sprite The sprite to add.
+	 */
+	public void addSprite(Sprite sprite) {
+		sprites.add(sprite);
+	}
+
+	/**
+	 * Adds a sprite to this group at the specified index, where index 0 would be the first to draw.
+	 * @param sprite The sprite.
+	 * @param index The index.
+	 */
+	public void addSprite(Sprite sprite, int index) {
+		sprites.add(index, sprite);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.steftmax.temol.graphics.Drawable#draw(com.steftmax.temol.graphics
+	 * .SpriteBatch)
+	 */
+	@Override
+	public void draw(SpriteBatch batch) {
+		for (Sprite sprite : sprites) {
+			batch.draw(sprite, sprite.pos.x + position.x, sprite.pos.y
+					+ position.y, sprite.width, sprite.height, scaleX
+					* sprite.scaleX, scaleY * sprite.scaleY,
+					flipX != sprite.flipX, flipY != sprite.flipY, sprite.color,
+					rotation, origin.x, origin.y);
+		}
+
+	}
+
+}
